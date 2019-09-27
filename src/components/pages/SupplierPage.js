29e7bc69a8b6c21cbc62db97ebcffb5c00d8fc3f
ganeshnayanajith <ganeshnayanajith40@gmail.com>
2019-09-27 20:25:38 +0530

    delete suppliers

--- src/components/pages/SupplierPage.js
+++ src/components/pages/SupplierPage.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import {MDBCard, MDBCol, MDBRow, MDBCardBody, MDBBtn, MDBDataTable, MDBContainer} from 'mdbreact';
 import FirebaseDB from "../../Firebase";
+import Swal from 'sweetalert';
 
 class SupplierPage extends React.Component {
 
     constructor(props) {
         super(props);
 
+        this.suppliersRef = "";
+
         this.onClick = this.onClick.bind(this);
         this.addItem = this.addItem.bind(this);
         this.viewItems = this.viewItems.bind(this);
         this.updateSupplier = this.updateSupplier.bind(this);
+        this.deleteSupplier = this.deleteSupplier.bind(this);
 
         this.state = {
             data: [],
@@ -56,25 +60,48 @@ class SupplierPage extends React.Component {
         console.log("click");
         this.props.history.push("/supplieradd");
     };
+
     addItem(e) {
         console.log(e.target.id);
-        this.props.history.push("/supplieradditem/"+e.target.id);
-    };
-    viewItems(e) {
-        console.log(e.target.id);
-        this.props.history.push("/supplierviewitems/"+e.target.id);
-    };
-    updateSupplier(e) {
-        console.log(e.target.id);
-        this.props.history.push("/supplierupdate/"+e.target.id);
+        this.props.history.push("/supplieradditem/" + e.target.id);
     };
 
+    viewItems(e) {
+        console.log(e.target.id);
+        this.props.history.push("/supplierviewitems/" + e.target.id);
+    };
+
+    updateSupplier(e) {
+        console.log(e.target.id);
+        this.props.history.push("/supplierupdate/" + e.target.id);
+    };
+
+    deleteSupplier(e) {
+        console.log(e.target.id);
+        let supplierId = e.target.id;
+
+
+        Swal({
+            title: "Are you sure?",
+            text: "Once deleted, you will not be able to recover this data",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        })
+            .then((willDelete) => {
+                if (willDelete) {
+
+                    this.suppliersRef.child(supplierId).remove();
+                    Swal("Success !", "Supplier Deleted Successfully !", "success");
+                }
+            });
+    };
 
 
     componentDidMount() {
         document.title = "Suppliers";
-        const suppliersRef = FirebaseDB.database().ref('suppliers');
-        suppliersRef.on('value', (snapshot) => {
+        this.suppliersRef = FirebaseDB.database().ref('suppliers');
+        this.suppliersRef.on('value', (snapshot) => {
             var suppliers = snapshot.val();
             var newsuppliers = [];
             for (let supplier in suppliers) {
@@ -83,10 +110,11 @@ class SupplierPage extends React.Component {
                     email: suppliers[supplier].email,
                     contact: suppliers[supplier].contact,
                     address: suppliers[supplier].address,
-                    add:<MDBBtn color="purple" size="sm" id={supplier} onClick={this.addItem}>Add Item</MDBBtn>,
-                    view:<MDBBtn color="green" size="sm" id={supplier} onClick={this.viewItems}>View Items</MDBBtn>,
-                    update:<MDBBtn color="yellow" size="sm" id={supplier} onClick={this.updateSupplier}>Update</MDBBtn>,
-                    delete:<MDBBtn color="red" size="sm" id={supplier} >Delete</MDBBtn>
+                    add: <MDBBtn color="purple" size="sm" id={supplier} onClick={this.addItem}>Add Item</MDBBtn>,
+                    view: <MDBBtn color="green" size="sm" id={supplier} onClick={this.viewItems}>View Items</MDBBtn>,
+                    update: <MDBBtn color="yellow" size="sm" id={supplier}
+                                    onClick={this.updateSupplier}>Update</MDBBtn>,
+                    delete: <MDBBtn color="red" size="sm" id={supplier} onClick={this.deleteSupplier}>Delete</MDBBtn>
                 });
             }
             this.setState({
@@ -113,10 +141,10 @@ class SupplierPage extends React.Component {
                     email: suppliers[supplier].email,
                     contact: suppliers[supplier].contact,
                     address: suppliers[supplier].address,
-                    add:<MDBBtn color="purple" size="sm" id={supplier} onClick={this.addItem}>Add Item</MDBBtn>,
-                    view:<MDBBtn color="green" size="sm" id={supplier} onClick={this.viewItems}>View Items</MDBBtn>,
-                    update:<MDBBtn color="yellow" size="sm" id={supplier} >Update</MDBBtn>,
-                    delete:<MDBBtn color="red" size="sm" id={supplier} >Delete</MDBBtn>
+                    add: <MDBBtn color="purple" size="sm" id={supplier} onClick={this.addItem}>Add Item</MDBBtn>,
+                    view: <MDBBtn color="green" size="sm" id={supplier} onClick={this.viewItems}>View Items</MDBBtn>,
+                    update: <MDBBtn color="yellow" size="sm" id={supplier}>Update</MDBBtn>,
+                    delete: <MDBBtn color="red" size="sm" id={supplier}>Delete</MDBBtn>
                 });
             }
             this.setState({
@@ -144,7 +172,7 @@ class SupplierPage extends React.Component {
                     </MDBCol>
                     <MDBCol sm="12" md="6" lg="4" className="mb-5">
                         <div className="d-flex justify-content-end">
-                            <MDBBtn color="success" onClick={this.onClick} >Add Supplier</MDBBtn>
+                            <MDBBtn color="success" onClick={this.onClick}>Add Supplier</MDBBtn>
                         </div>
 
                     </MDBCol>
